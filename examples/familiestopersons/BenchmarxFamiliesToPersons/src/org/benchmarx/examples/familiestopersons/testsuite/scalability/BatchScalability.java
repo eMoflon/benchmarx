bbb5b49afc8485a374dc1143a1523a1cb28d99a5
Chandni Shah <crshah@mail.upb.de>
2016-12-06 15:56:41 +0100

    measurements for 5 times creating family

--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/BatchScalability.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/BatchScalability.java
@@ -1,5 +1,8 @@
 package org.benchmarx.examples.familiestopersons.testsuite.scalability;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 import org.benchmarx.BXToolTimer;
 import org.benchmarx.examples.familiestopersons.families.core.FamilyHelper;
 import org.benchmarx.examples.familiestopersons.implementations.bigul.BiGULFamiliesToPersons;
@@ -16,13 +19,52 @@ public class BatchScalability {
 	private static PersonHelper helperPersons = new PersonHelper();
 	
 	public static void main(String[] args) {
+		System.out.println("...........................................");
+		
+		System.out.println("Time for creating single (simpson) family 1 time");
+		
 		BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer1 = new BXToolTimer<>(new EMoflonFamiliesToPersons());
-		System.out.print(timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
+		System.out.println("eMoflon = "+timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
 		
 		BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer2 = new BXToolTimer<>(new MediniQVTFamiliesToPersons());
-		System.out.print(timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
+		System.out.println("MediniQVT = "+timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
 		
 		BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer = new BXToolTimer<>(new BiGULFamiliesToPersons());
-		System.out.print(timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms");
+		System.out.println("BiGIL = "+timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
+	
+		System.out.println("...........................................");
+		System.out.println("Time for creating single (simpson) family 5 times");
+		
+		long sumeMoflon = 0;
+		long sumMedini = 0;
+		long sumBigul = 0;
+		
+		for(int i=0;i<4;i++)
+		{
+			sumeMoflon = sumeMoflon + timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily); 
+			sumMedini = sumMedini + timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily);
+			sumBigul = sumBigul + timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily);
+		}
+		
+		System.out.println("eMoflon ="+sumeMoflon);
+		System.out.println("MediniQVT ="+sumMedini);
+		System.out.println("BiGIL ="+sumBigul);
+		
+		
+			
+		
+		/*String fileTimingResults = "output.txt";
+		
+		try {
+			PrintWriter outputStream = new PrintWriter(fileTimingResults);
+			outputStream.println("eMoflon = "+timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily)+"ms; MediniQVT = "+timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily)+"ms; BiGUL = "+timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily)+"ms;");
+			outputStream.flush();
+			outputStream.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}*/
+		
+	
 	}
 }
