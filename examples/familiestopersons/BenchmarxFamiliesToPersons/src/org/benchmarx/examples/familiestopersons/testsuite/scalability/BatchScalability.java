2dfb64f250fbc7226b6ab9063d447523ff48c965
Chandni Shah <crshah@mail.upb.de>
2016-12-07 01:32:54 +0100

    runtime analysis done for (30 families and 120 members) measured 5 times

--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/BatchScalability.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/BatchScalability.java
@@ -1,7 +1,8 @@
 package org.benchmarx.examples.familiestopersons.testsuite.scalability;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import org.benchmarx.BXToolTimer;
 import org.benchmarx.examples.familiestopersons.families.core.FamilyHelper;
@@ -19,52 +20,75 @@ public class BatchScalability {
 	private static PersonHelper helperPersons = new PersonHelper();
 	
 	public static void main(String[] args) {
-		System.out.println("...........................................");
-		
-		System.out.println("Time for creating single (simpson) family 1 time");
-		
 		BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer1 = new BXToolTimer<>(new EMoflonFamiliesToPersons());
-		System.out.println("eMoflon = "+timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
-		
 		BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer2 = new BXToolTimer<>(new MediniQVTFamiliesToPersons());
-		System.out.println("MediniQVT = "+timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
-		
 		BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer = new BXToolTimer<>(new BiGULFamiliesToPersons());
-		System.out.println("BiGIL = "+timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily) + "ms;");
-	
-		System.out.println("...........................................");
-		System.out.println("Time for creating single (simpson) family 5 times");
 		
-		long sumeMoflon = 0;
-		long sumMedini = 0;
-		long sumBigul = 0;
-		
-		for(int i=0;i<4;i++)
-		{
-			sumeMoflon = sumeMoflon + timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily); 
-			sumMedini = sumMedini + timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily);
-			sumBigul = sumBigul + timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily);
-		}
-		
-		System.out.println("eMoflon ="+sumeMoflon);
-		System.out.println("MediniQVT ="+sumMedini);
-		System.out.println("BiGIL ="+sumBigul);
-		
-		
-			
-		
-		/*String fileTimingResults = "output.txt";
+		String commaDeli = ",";
+		String newLineSep = "\n";
+		String fileHeader = " , , , ,eMoflon,Medini,BiGUL";
 		
 		try {
-			PrintWriter outputStream = new PrintWriter(fileTimingResults);
-			outputStream.println("eMoflon = "+timer1.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily)+"ms; MediniQVT = "+timer2.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily)+"ms; BiGUL = "+timer.timeSourceEditFromScratchInMS(helperFamily::createSimpsonFamily)+"ms;");
-			outputStream.flush();
-			outputStream.close();
+			FileWriter testWriter = new FileWriter(new File("test2.csv"));
+			testWriter.append(fileHeader);
 			
-		} catch (FileNotFoundException e) {
+			for(int k=1;k<=5;k++)
+			{	
+				testWriter.append(newLineSep);
+				testWriter.append(newLineSep);
+				testWriter.append("measurement "+String.valueOf(k)+" time");
+				
+				int i= 0;
+				while(i<=30)
+				{
+					
+					long sumeMoflon = 0;
+					long sumMedini = 0;
+					long sumBigul = 0;
+					
+					if(i==0)
+					{
+						sumeMoflon = timer1.timeSourceEditFromScratchInMS(helperFamily::createNewfamilyBachchanWithMembers); 
+						sumMedini = timer2.timeSourceEditFromScratchInMS(helperFamily::createNewfamilyBachchanWithMembers);
+						sumBigul = timer.timeSourceEditFromScratchInMS(helperFamily::createNewfamilyBachchanWithMembers);
+						
+						testWriter.append(newLineSep);
+						testWriter.append("1 Famliy and 4 members created");
+						
+					}
+					for(int j=0;j<i;j++)
+					{
+						sumeMoflon = sumeMoflon + timer1.timeSourceEditFromScratchInMS(helperFamily::createNewfamilyBachchanWithMembers); 
+						sumMedini = sumMedini + timer2.timeSourceEditFromScratchInMS(helperFamily::createNewfamilyBachchanWithMembers);
+						sumBigul = sumBigul + timer.timeSourceEditFromScratchInMS(helperFamily::createNewfamilyBachchanWithMembers);
+					}
+					
+					if(i!=0)
+					{
+						testWriter.append(newLineSep);
+						testWriter.append(String.valueOf(i)+" Famliies and "+String.valueOf(i*4) +" members created");
+					}
+					
+					testWriter.append(commaDeli);
+					testWriter.append(commaDeli);
+					testWriter.append(commaDeli);
+					testWriter.append(commaDeli);
+					testWriter.append(String.valueOf(sumeMoflon));
+					testWriter.append(commaDeli);
+					testWriter.append(String.valueOf(sumMedini));
+					testWriter.append(commaDeli);
+					testWriter.append(String.valueOf(sumBigul));
+					
+				i=i+5;
+				}
+			}	
+			testWriter.flush();
+			testWriter.close();	
+			System.out.println("csv created successfully!!");
+			
+		} catch (IOException e) {
 			e.printStackTrace();
-		}*/
+		}
 		
-	
 	}
 }
