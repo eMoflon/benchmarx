605f1bedb40622427e92f55a66d57492da8f738b
Anthony Anjorin <tonyanjorin@googlemail.com>
2017-01-02 17:25:43 +0100

    Improved runtime measurement for batch case

--- core/Benchmarx/src/org/benchmarx/BXToolTimer.java
+++ core/Benchmarx/src/org/benchmarx/BXToolTimer.java
@@ -1,18 +1,35 @@
 package org.benchmarx;
 
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BXToolTimer<S, T, D> {
 	
 	private BXTool<S,T,D> tool;
+	private final int REPEAT;
 	
-	public BXToolTimer(BXTool<S, T, D> tool){
+	public BXToolTimer(BXTool<S, T, D> tool, int repeat){
 		this.tool = tool;
+		this.REPEAT = repeat;
 	}
 	
 	private long timeFromScratch(Runnable action){
 		tool.initiateSynchronisationDialogue();
 		return timeAction(action);
+	}
+
+	private long median(Supplier<Long> measurement) {		
+		List<Long> measurements = 
+				Stream.generate(measurement)
+					 .limit(REPEAT)
+					 .sorted()
+					 .collect(Collectors.toList());
+		
+		//System.out.print(measurements);
+		return measurements.get(REPEAT/2);
 	}
 
 	private long timeAction(Runnable action) {
@@ -29,7 +46,7 @@ public class BXToolTimer<S, T, D> {
 	}
 	
 	public long timeSourceEditFromScratchInMS(Consumer<S> edit){
-		return timeFromScratch(() -> tool.performAndPropagateSourceEdit(edit));
+		return median(() -> timeFromScratch(() -> tool.performAndPropagateSourceEdit(edit)));
 	}
 	
 	public double timeSourceEditFromScratchInS(Consumer<S> edit){
@@ -37,7 +54,7 @@ public class BXToolTimer<S, T, D> {
 	}
 	
 	public long timeTargetEditFromScratchInMS(Consumer<T> edit){
-		return timeFromScratch(() -> tool.performAndPropagateTargetEdit(edit));
+		return median(() -> timeFromScratch(() -> tool.performAndPropagateTargetEdit(edit)));
 	}
 	
 	public double timeTargetEditFromScratchInS(Consumer<T> edit){
@@ -58,5 +75,9 @@ public class BXToolTimer<S, T, D> {
 		tool.initiateSynchronisationDialogue();
 		setup.run();
 		return timeAction(action);
+	}
+
+	public String getName() {
+		return tool.getName();
 	}
 }
--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/BatchScalability.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/BatchScalability.java
@@ -1,5 +1,6 @@
 package org.benchmarx.examples.familiestopersons.testsuite.scalability;
 
+import org.benchmarx.BXTool;
 import org.benchmarx.BXToolTimer;
 import org.benchmarx.examples.familiestopersons.implementations.bigul.BiGULFamiliesToPersons;
 import org.benchmarx.examples.familiestopersons.implementations.emoflon.EMoflonFamiliesToPersons;
@@ -15,21 +16,40 @@ import Persons.PersonRegister;
 import Persons.PersonsFactory;
 
 public class BatchScalability {
-	private final int NO_OF_FAMILIES;
-	private final int NO_OF_PERSONS;
-	private final int REPEAT;
+	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool1 = new BiGULFamiliesToPersons();
+	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool2 = new EMoflonFamiliesToPersons();
+	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool3 = new MediniQVTFamiliesToPersons();
 	
-	public BatchScalability(int numberOfFamilies, int numberOfPersons, int repeat) {
+	private final int NO_OF_FAMILIES;
+	private final int NO_OF_CHILDREN;
+	private final int NO_OF_ELEMENTS;
+	
+	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer1;
+	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer2;
+	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer3;
+	
+	public BatchScalability(int numberOfFamilies, int noOfChildren, int repeat) {
 		this.NO_OF_FAMILIES = numberOfFamilies;
-		this.NO_OF_PERSONS = numberOfPersons;
-		this.REPEAT = repeat;
+		this.NO_OF_CHILDREN = noOfChildren;
+		this.NO_OF_ELEMENTS = NO_OF_FAMILIES * (NO_OF_CHILDREN + 2);
+		
+		timer1 = new BXToolTimer<>(tool1, repeat);
+		timer2 = new BXToolTimer<>(tool2, repeat);
+		timer3 = new BXToolTimer<>(tool3, repeat);;
 	}
 	
 	public PersonRegister createPersons(PersonRegister register){
-		for(int i = 0; i < NO_OF_PERSONS; i++){
-			Person person = PersonsFactory.eINSTANCE.createFemale();
-			person.setName("Doe, Jane_" + i);
-			register.getPersons().add(person);
+		for(int i = 0; i < NO_OF_FAMILIES; i++){
+			Person mother = PersonsFactory.eINSTANCE.createFemale();
+			mother.setName("Doe_" + i + ", Jane");
+			Person father = PersonsFactory.eINSTANCE.createMale();
+			father.setName("Doe_" + i + ", John");
+			
+			for (int j = 0; j < NO_OF_CHILDREN; j++) {
+				Person person = Math.random() < 0.5 ? PersonsFactory.eINSTANCE.createFemale() : PersonsFactory.eINSTANCE.createMale();
+				person.setName("Doe_" + i + ", Child_" + i);
+				register.getPersons().add(person);
+			}
 		}
 		
 		return register;
@@ -38,56 +58,78 @@ public class BatchScalability {
 	public FamilyRegister createFamiliesWithMembers(FamilyRegister register){
 		for (int i = 0; i < NO_OF_FAMILIES; i++) {
 			Family family = FamiliesFactory.eINSTANCE.createFamily();
-			family.setName("Simpson_" + i);
+			family.setName("Doe_" + i);
 			register.getFamilies().add(family);
 
-			FamilyMember familyMother = FamiliesFactory.eINSTANCE.createFamilyMember();
-			familyMother.setName("Marge");
-			family.setMother(familyMother);
-
-			FamilyMember familySon = FamiliesFactory.eINSTANCE.createFamilyMember();
-			familySon.setName("Bart");
-			family.getSons().add(familySon);
-
-			FamilyMember familyDaughterOne = FamiliesFactory.eINSTANCE.createFamilyMember();
-			familyDaughterOne.setName("Lisa");
-			family.getDaughters().add(familyDaughterOne);
-
-			FamilyMember familyDaughterTwo = FamiliesFactory.eINSTANCE.createFamilyMember();
-			familyDaughterTwo.setName("Maggie");
-			family.getDaughters().add(familyDaughterTwo);
+			{
+				FamilyMember familyMother = FamiliesFactory.eINSTANCE.createFamilyMember();
+				familyMother.setName("Jane");
+				family.setMother(familyMother);
+			}
+			
+			{
+				FamilyMember familyFather = FamiliesFactory.eINSTANCE.createFamilyMember();
+				familyFather.setName("John");
+				family.setFather(familyFather);
+			}
+			
+			for (int j = 0; j < NO_OF_CHILDREN; j++) {
+				FamilyMember child = FamiliesFactory.eINSTANCE.createFamilyMember();
+				child.setName("Child_" + j);
+				
+				if(Math.random() < 0.5)
+					family.getDaughters().add(child);	
+				else
+					family.getSons().add(child);
+			}
 		}
 		
 		return register;
 	}
 	
-	public void runMeasurements() {
-		for (int i = 0; i < REPEAT; i++) {
-			System.out.println();
-			System.out.println("(" + NO_OF_FAMILIES + ", " + NO_OF_PERSONS + "), " + (i+1) + " of " + REPEAT);
-			
-			BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer1 = new BXToolTimer<>(new EMoflonFamiliesToPersons());
-			System.out.print("eMoflon: ");
-			System.out.print(timer1.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + "s;");
-			System.out.println(timer1.timeTargetEditFromScratchInS(this::createPersons) + "s;");
+	private void runFWDMeasurements(){
+		System.out.print(NO_OF_ELEMENTS + ";");
+		System.out.print(timer1.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + "s;");
+		System.out.print(timer2.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + "s;");
+		System.out.print(timer3.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + "s");
+		System.out.println();
+	}
+	
+	private void runBWDMeasurements(){
+		System.out.print(NO_OF_ELEMENTS + ";");
+		System.out.print(timer1.timeTargetEditFromScratchInS(this::createPersons) + "s;");
+		System.out.print(timer2.timeTargetEditFromScratchInS(this::createPersons) + "s;");
+		System.out.print(timer3.timeTargetEditFromScratchInS(this::createPersons) + "s");
+		System.out.println();
+	}
 
-			BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer2 = new BXToolTimer<>(new BiGULFamiliesToPersons());
-			System.out.print("BiGUL:   ");
-			System.out.print(timer2.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + "s;");
-			System.out.println(timer2.timeTargetEditFromScratchInS(this::createPersons) + "s;");
-
-			BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer3 = new BXToolTimer<>(new MediniQVTFamiliesToPersons());
-			System.out.print("Medini:  ");
-			System.out.print(timer3.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + "s;");
-			System.out.println(timer3.timeTargetEditFromScratchInS(this::createPersons) + "s;");			
-		}
+	private static void runFWDMeasurements(int numOfFamilies, int numOfChildren, int repetitions) {
+		new BatchScalability(numOfFamilies, numOfChildren, repetitions).runFWDMeasurements();
+	}
+	
+	private static void runBWDMeasurements(int numOfFamilies, int numOfChildren, int repetitions) {
+		new BatchScalability(numOfFamilies, numOfChildren, repetitions).runBWDMeasurements();
+	}
+	
+	private static void printHeader() {
+		System.out.println("model size (# of nodes and edges);" +  tool1.getName() + ";" +  tool2.getName() + ";" +  tool3.getName());
 	}
 	
 	public static void main(String[] args) {
-		BatchScalability scalabilityTest = new BatchScalability(1000, 1000, 5);
-		scalabilityTest.runMeasurements();
+		printHeader();
+		runFWDMeasurements(100, 3, 21);
+		runFWDMeasurements(500, 3, 5);
+		runFWDMeasurements(1000, 3, 5);
+		runFWDMeasurements(10000, 3, 3);
+		runFWDMeasurements(50000, 3, 1);
+		runFWDMeasurements(100000, 3, 1);
 		
-		scalabilityTest = new BatchScalability(5000, 5000, 5);
-		scalabilityTest.runMeasurements();
+		printHeader();
+		runBWDMeasurements(100, 3, 21);
+		runBWDMeasurements(500, 3, 5);
+		runBWDMeasurements(1000, 3, 5);
+		runBWDMeasurements(10000, 3, 3);
+		runBWDMeasurements(50000, 3, 1);
+		runBWDMeasurements(100000, 3, 1);
 	}
 }
