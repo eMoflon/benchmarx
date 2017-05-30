c5851a66e53c77f74304e9e27a747c7e1960f8f0
Georg Hinkel <hinkel@fzi.de>
2017-05-30 20:09:06 +0200

    Add missing file changes (Eclipse does not auto-save)

--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/ScalabilityMeasurements.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/ScalabilityMeasurements.java
@@ -19,11 +19,12 @@ import Persons.PersonRegister;
 import Persons.PersonsFactory;
 
 public class ScalabilityMeasurements {
-	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool1 = new NMFFamiliesToPersonsIncremental();
+	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool1 = new BiGULFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool2 = new EMoflonFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool3 = new MediniQVTFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool4 = new UbtXtendFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool5 = new FunnyQTFamiliesToPerson();
+	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool6 = new NMFFamiliesToPersonsIncremental();
 	
 	private static final String DELIMITER = ",";
 	private static final String UNIT = "";
@@ -37,6 +38,7 @@ public class ScalabilityMeasurements {
 	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer3;
 	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer4;
 	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer5;
+	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer6;
 	
 	public ScalabilityMeasurements(int numberOfFamilies, int noOfChildren, int repeat) {
 		this.NO_OF_FAMILIES = numberOfFamilies;
@@ -53,6 +55,7 @@ public class ScalabilityMeasurements {
 		timer3 = new BXToolTimer<>(tool3, repeat);;
 		timer4 = new BXToolTimer<>(tool4, repeat);
 		timer5 = new BXToolTimer<>(tool5, repeat);
+		timer6 = new BXToolTimer<>(tool6, repeat);
 	}
 	
 	public void createPersons(PersonRegister register){
@@ -121,7 +124,8 @@ public class ScalabilityMeasurements {
 		System.out.print(timer2.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
 		System.out.print(timer3.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
 		System.out.print(timer4.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
-		System.out.print(timer5.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT);
+		System.out.print(timer5.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
+		System.out.print(timer6.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT);
 		System.out.println();
 	}
 	
@@ -131,7 +135,8 @@ public class ScalabilityMeasurements {
 		System.out.print(timer2.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
 		System.out.print(timer3.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
 		System.out.print(timer4.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
-		System.out.print(timer5.timeTargetEditFromScratchInS(this::createPersons) + UNIT);
+		System.out.print(timer5.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
+		System.out.print(timer6.timeTargetEditFromScratchInS(this::createPersons) + UNIT);
 		System.out.println();
 	}
 	
@@ -141,7 +146,8 @@ public class ScalabilityMeasurements {
 		System.out.print(timer2.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
 		System.out.print(timer3.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
 		System.out.print(timer4.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
-		System.out.print(timer5.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT);
+		System.out.print(timer5.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
+		System.out.print(timer6.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT);
 		System.out.println();
 	}
 	
@@ -151,7 +157,8 @@ public class ScalabilityMeasurements {
 		System.out.print(timer2.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
 		System.out.print(timer3.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
 		System.out.print(timer4.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
-		System.out.print(timer5.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT);
+		System.out.print(timer5.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
+		System.out.print(timer6.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT);
 		System.out.println();
 	}
 
@@ -180,7 +187,8 @@ public class ScalabilityMeasurements {
                 + tool2.getName() + DELIMITER
                 + tool3.getName() + DELIMITER 
                 + tool4.getName() + DELIMITER
-                + tool5.getName());
+                + tool5.getName() + DELIMITER
+                + tool6.getName());
     }
 	
 	public static void main(String[] args) {
