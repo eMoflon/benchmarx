b695a58f5cdb4ae061d807b1f8b3a23de40e9c13
Patrick Robrecht <mail@patrick-robrecht.de>
2018-02-23 12:25:15 +0100

    Fix warnings in the workspace (injection folder if eMoflon EMF Project builder is used)

--- examples/familiestopersons/metamodels/Persons/gen/Persons/impl/FemaleImpl.java
+++ examples/familiestopersons/metamodels/Persons/gen/Persons/impl/FemaleImpl.java
@@ -13,6 +13,8 @@ import org.eclipse.emf.ecore.EClass;
  * <!-- begin-user-doc -->
  * An implementation of the model object '<em><b>Female</b></em>'.
  * <!-- end-user-doc -->
+ * <p>
+ * </p>
  *
  * @generated
  */
--- examples/familiestopersons/metamodels/Persons/gen/Persons/impl/PersonRegisterImpl.java
+++ examples/familiestopersons/metamodels/Persons/gen/Persons/impl/PersonRegisterImpl.java
@@ -28,10 +28,10 @@ import org.eclipse.emf.ecore.util.InternalEList;
  * <!-- end-user-doc -->
  * <p>
  * The following features are implemented:
- * </p>
  * <ul>
  *   <li>{@link Persons.impl.PersonRegisterImpl#getPersons <em>Persons</em>}</li>
  * </ul>
+ * </p>
  *
  * @generated
  */
