52c80ae6e499ef9847512c2a9264f38eaf05f323
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-07-23 19:09:13 +0200

    Added ibex solution for f2p benchmarx

---
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/implementations/ibextgg/F2PUpdatePolicy.java
@@ -0,0 +1,78 @@
+package org.benchmarx.examples.familiestopersons.implementations.ibextgg;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.benchmarx.Configurator;
+import org.benchmarx.examples.familiestopersons.testsuite.Decisions;
+import org.emoflon.ibex.tgg.compiler.patterns.PatternSuffixes;
+import org.emoflon.ibex.tgg.operational.matches.IMatch;
+import org.emoflon.ibex.tgg.operational.matches.ImmutableMatchContainer;
+import org.emoflon.ibex.tgg.operational.updatepolicy.UpdatePolicy;
+import org.emoflon.ibex.tgg.util.MAUtil;
+
+
+public class F2PUpdatePolicy extends UpdatePolicy {
+
+	private Configurator<Decisions> configurator;
+	
+	public F2PUpdatePolicy(Configurator<Decisions> configurator) {
+		this.configurator = configurator;
+	}
+
+	@Override
+	public IMatch chooseOneMatch(ImmutableMatchContainer matchContainer) {
+		Set<IMatch> matches = new HashSet<IMatch>(matchContainer.getMatches());
+		matches.removeIf(this::isIrrelevantMatchForSync);
+				
+		handlePrefsForExistingFamily(matches);
+		handlePrefsForParents(matches);
+
+		return matches.isEmpty()? matchContainer.getNextRandom() : matches.iterator().next();
+	}
+
+	private boolean isIrrelevantMatchForSync(IMatch m) {
+		return m.getPatternName().endsWith(PatternSuffixes.CONSISTENCY);
+	}
+
+	private void handlePrefsForExistingFamily(Set<IMatch> matches) {
+		if (configurator.decide(Decisions.PREFER_EXISTING_FAMILY_TO_NEW)) {
+			if(thereIsAtLeastOneExistingFamily(matches))
+				matches.removeIf(m -> !usesExistingFamily(m));
+		} else {
+			matches.removeIf(this::usesExistingFamily);
+		}
+	}
+
+	private boolean thereIsAtLeastOneExistingFamily(Set<IMatch> matches) {
+		return matches.stream().anyMatch(this::usesExistingFamily);
+	}
+	
+	private boolean usesExistingFamily(IMatch m) {
+		return !m.getPatternName().contains(MAUtil.FUSED);
+	}
+
+	private void handlePrefsForParents(Set<IMatch> matches) {
+		if (configurator.decide(Decisions.PREFER_CREATING_PARENT_TO_CHILD)) {
+			if(atLeastOneParentCanBeCreated(matches))
+				matches.removeIf(this::isChildRule);
+		} else if(atLeastOneChildCanBeCreated(matches))
+			matches.removeIf(this::isParentRule);
+	}
+	
+	private boolean atLeastOneParentCanBeCreated(Set<IMatch> matches) {
+		return matches.stream().anyMatch(this::isParentRule);
+	}
+
+	private boolean isParentRule(IMatch m) {
+		return m.getPatternName().contains("Mother") || m.getPatternName().contains("Father");
+	}
+
+	private boolean isChildRule(IMatch m) {
+		return m.getPatternName().contains("Son") || m.getPatternName().contains("Daughter");
+	}
+
+	private boolean atLeastOneChildCanBeCreated(Set<IMatch> matches) {
+		return matches.stream().anyMatch(this::isChildRule);
+	}
+}
---
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/implementations/ibextgg/IBeXTGGFamiliesToPersons.java
@@ -0,0 +1,116 @@
+package org.benchmarx.examples.familiestopersons.implementations.ibextgg;
+
+import java.io.IOException;
+import java.util.function.Consumer;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.benchmarx.Configurator;
+import org.benchmarx.emf.BXToolForEMF;
+import org.benchmarx.examples.familiestopersons.testsuite.Decisions;
+import org.benchmarx.families.core.FamiliesComparator;
+import org.benchmarx.persons.core.PersonsComparator;
+import org.emoflon.ibex.tgg.run.ibextggfamiliestopersons.SYNC_App;
+
+import Families.FamiliesFactory;
+import Families.FamilyRegister;
+import Persons.PersonRegister;
+
+public class IBeXTGGFamiliesToPersons
+		extends BXToolForEMF<FamilyRegister, PersonRegister, org.benchmarx.examples.familiestopersons.testsuite.Decisions> {
+
+	private SYNC_App sync;
+
+	public IBeXTGGFamiliesToPersons() {
+		super(new FamiliesComparator(), new PersonsComparator());
+
+		BasicConfigurator.configure();
+		Logger.getRootLogger().setLevel(Level.ERROR);
+	}
+
+	@Override
+	public String getName() {
+		return "IBeX-TGG";
+	}
+
+	@Override
+	public void initiateSynchronisationDialogue() {
+		try {
+			sync = new SYNC_App();
+			BasicConfigurator.configure();
+			Logger.getRootLogger().setLevel(Level.DEBUG);
+
+			// Create initial src and trg models
+			FamilyRegister o = FamiliesFactory.eINSTANCE.createFamilyRegister();
+			sync.getSourceResource().getContents().add(o);
+
+			sync.forward();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void performAndPropagateSourceEdit(Consumer<FamilyRegister> edit) {
+		// Adapt source model
+		FamilyRegister o = (FamilyRegister) sync.getSourceResource().getContents().get(0);
+		edit.accept(o);
+
+		// Invoke sync
+		try {
+			sync.forward();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void performAndPropagateTargetEdit(Consumer<PersonRegister> edit) {
+		// Adapt target model
+		PersonRegister o = (PersonRegister) sync.getTargetResource().getContents().get(0);
+		edit.accept(o);
+
+		// Invoke sync
+		try {
+			sync.backward();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void performIdleSourceEdit(Consumer<FamilyRegister> edit) {
+		performAndPropagateSourceEdit(edit);
+	}
+
+	@Override
+	public void performIdleTargetEdit(Consumer<PersonRegister> edit) {
+		performAndPropagateTargetEdit(edit);
+	}
+
+	@Override
+	public void setConfigurator(Configurator<Decisions> configurator) {
+		sync.setUpdatePolicy(new F2PUpdatePolicy(configurator));
+	}
+
+	@Override
+	public FamilyRegister getSourceModel() {
+		return (FamilyRegister) sync.getSourceResource().getContents().get(0);
+	}
+
+	@Override
+	public PersonRegister getTargetModel() {
+		return (PersonRegister) sync.getTargetResource().getContents().get(0);
+	}
+
+	@Override
+	public void saveModels(String name) {
+		try {
+			sync.saveModels();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
