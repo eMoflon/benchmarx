6427af3c9cdbeb721439b2cb96d53089e82a1155
Anthony Anjorin <tonyanjorin@googlemail.com>
2018-07-24 23:58:13 +0200

    Cleaned up benchmarx structure

---
+++ examples/pdb1topdb2/implementationArtefacts/IBeXTGGPDB1ToPDB2/src/org/emoflon/ibex/tgg/run/ibextggpdb1topdb2/BWD_OPT_App.java
@@ -0,0 +1,48 @@
+package org.emoflon.ibex.tgg.run.ibextggpdb1topdb2;
+
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.operational.strategies.opt.BWD_OPT;
+import org.emoflon.ibex.tgg.runtime.engine.DemoclesTGGEngine;
+
+public class BWD_OPT_App extends BWD_OPT {
+
+	public BWD_OPT_App() throws IOException {
+		super(createIbexOptions());
+		registerBlackInterpreter(new DemoclesTGGEngine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+		Logger.getRootLogger().setLevel(Level.INFO);
+
+		BWD_OPT_App bwd_opt = new BWD_OPT_App();
+		
+		logger.info("Starting BWD_OPT");
+		long tic = System.currentTimeMillis();
+		bwd_opt.run();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed BWD_OPT in: " + (toc - tic) + " ms");
+		
+		bwd_opt.saveModels();
+		bwd_opt.terminate();
+	}
+	
+	
+	@Override
+	protected void registerUserMetamodels() throws IOException {
+		_RegistrationHelper.registerMetamodels(rs, this);
+			
+		// Register correspondence metamodel last
+		loadAndRegisterMetamodel(options.projectPath() + "/model/" + options.projectName() + ".ecore");
+	}
+	
+	private static IbexOptions createIbexOptions() {
+		return _RegistrationHelper.createIbexOptions();
+	}
+}
---
+++ examples/pdb1topdb2/implementationArtefacts/IBeXTGGPDB1ToPDB2/src/org/emoflon/ibex/tgg/run/ibextggpdb1topdb2/MODELGEN_App.java
@@ -0,0 +1,54 @@
+package org.emoflon.ibex.tgg.run.ibextggpdb1topdb2;
+
+import java.io.IOException;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
+import org.emoflon.ibex.tgg.operational.defaults.IbexOptions;
+import org.emoflon.ibex.tgg.operational.strategies.gen.MODELGEN;
+import org.emoflon.ibex.tgg.operational.strategies.gen.MODELGENStopCriterion;
+import org.emoflon.ibex.tgg.runtime.engine.DemoclesTGGEngine;
+
+public class MODELGEN_App extends MODELGEN {
+
+	public MODELGEN_App() throws IOException {
+		super(createIbexOptions());
+		registerBlackInterpreter(new DemoclesTGGEngine());
+	}
+
+	public static void main(String[] args) throws IOException {
+		BasicConfigurator.configure();
+		Logger.getRootLogger().setLevel(Level.INFO);
+
+		MODELGEN_App generator = new MODELGEN_App();
+		
+		MODELGENStopCriterion stop = new MODELGENStopCriterion(generator.getTGG());
+		stop.setTimeOutInMS(1000);
+		generator.setStopCriterion(stop);
+		
+		logger.info("Starting MODELGEN");
+		long tic = System.currentTimeMillis();
+		generator.run();
+		long toc = System.currentTimeMillis();
+		logger.info("Completed MODELGEN in: " + (toc - tic) + " ms");
+		
+		
+		generator.saveModels();
+		generator.terminate();
+	}
+	
+	
+	@Override
+	protected void registerUserMetamodels() throws IOException {
+		_RegistrationHelper.registerMetamodels(rs, this);
+			
+		// Register correspondence metamodel last
+		loadAndRegisterMetamodel(options.projectPath() + "/model/" + options.projectName() + ".ecore");
+	}
+	
+	private static IbexOptions createIbexOptions() {
+		return _RegistrationHelper.createIbexOptions();
+	}
+}
