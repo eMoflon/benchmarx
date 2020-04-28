539bf4590301862c57c3553c485067b1a57f776d
Arikae <arikae00@jesitec.de>
2020-04-28 13:35:50 +0200

    fixed cpm and gantt genmodel (broken modelpluginid)

---
+++ examples/gantttocpm/metamodels/de.ubt.ai1.m2m.cpm/src/cpm/CpmFactory.java
@@ -0,0 +1,60 @@
+/**
+ */
+package cpm;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see cpm.CpmPackage
+ * @generated
+ */
+public interface CpmFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	CpmFactory eINSTANCE = cpm.impl.CpmFactoryImpl.init();
+
+	/**
+	 * Returns a new object of class '<em>CPM Network</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>CPM Network</em>'.
+	 * @generated
+	 */
+	CPMNetwork createCPMNetwork();
+
+	/**
+	 * Returns a new object of class '<em>Event</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Event</em>'.
+	 * @generated
+	 */
+	Event createEvent();
+
+	/**
+	 * Returns a new object of class '<em>Activity</em>'.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return a new object of class '<em>Activity</em>'.
+	 * @generated
+	 */
+	Activity createActivity();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	CpmPackage getCpmPackage();
+
+} //CpmFactory
