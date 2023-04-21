ced698c43cdf74511b2ed20f8aab511da722e191
Antonio García-Domínguez <a.garcia-dominguez@york.ac.uk>
2023-04-21 09:28:17 +0100

    MiniYAMLComparator: specify whole YAML path in test assertions

--- examples/containerstominiyaml/BenchmarxContainersToMiniYAML/src/org/benchmarx/examples/containerstominiyaml/comparators/MiniYAMLComparator.java
+++ examples/containerstominiyaml/BenchmarxContainersToMiniYAML/src/org/benchmarx/examples/containerstominiyaml/comparators/MiniYAMLComparator.java
@@ -19,48 +19,59 @@ import miniyaml.MapEntry;
 import miniyaml.Scalar;
 import miniyaml.Value;
 
+/**
+ * MiniYAML model comparator which ignores map entry and list element order.
+ */
 public class MiniYAMLComparator implements Comparator<miniyaml.Map> {
 
 	@Override
 	public void assertEquals(Map expected, Map actual) {
+		assertEquals("", expected, actual);
+	}
+
+	private void assertEquals(String path, Map expected, Map actual) {
 		java.util.Map<String, Value> expectedEntries = getEntries(expected);
 		java.util.Map<String, Value> actualEntries = getEntries(actual);
 		for (Entry<String, Value> expectedEntry : expectedEntries.entrySet()) {
 			Value actualValue = actualEntries.remove(expectedEntry.getKey());
-			assertEquals("Value for " + expectedEntry.getKey() + " should be the same", expectedEntry.getValue(), actualValue);
+			assertEquals(
+				path + (path.length() > 0 ? "." : "") + expectedEntry.getKey(),
+				expectedEntry.getValue(), actualValue);
 		}
-		assertTrue("Actual map should have the same keys as the expected map, but it also had " + actualEntries.keySet(), actualEntries.isEmpty());
+		assertTrue(
+			String.format("Actual map at '%s' should have the same keys as the expected map, but it also had %s", path, actualEntries.keySet()),
+			actualEntries.isEmpty());
 	}
 
-	private void assertEquals(String desc, Value expected, Value actual) {
+	private void assertEquals(String path, Value expected, Value actual) {
 		if (expected == null) {
-			assertNull(desc + ": should not have a value", actual);
+			assertNull(path + ": should not have a value", actual);
 		} else {
-			assertNotNull(desc + ": should have a value", actual);
+			assertNotNull(path + ": should have a value", actual);
 
-			Assert.assertEquals(desc + ": should be of the same type",
+			Assert.assertEquals(path + ": should be of the same type",
 				expected.getClass().getCanonicalName(), actual.getClass().getCanonicalName());
 
 			if (expected instanceof Map) {
-				assertEquals((Map) expected, (Map) actual);
+				assertEquals(path, (Map) expected, (Map) actual);
 			} else if (expected instanceof miniyaml.List) {
-				assertEquals((miniyaml.List) expected, (miniyaml.List) actual);
+				assertEquals(path, (miniyaml.List) expected, (miniyaml.List) actual);
 			} else if (expected instanceof miniyaml.Scalar) {
-				assertEquals((miniyaml.Scalar) expected, (miniyaml.Scalar) actual);  
+				assertEquals(path, (miniyaml.Scalar) expected, (miniyaml.Scalar) actual);
 			} else {
 				fail("Unexpected type " + expected.getClass().getCanonicalName());
 			}
 		}
 	}
 
-	private void assertEquals(Scalar expected, Scalar actual) {
-		Assert.assertEquals("Scalar values should be the same", expected.getValue(), actual.getValue());
+	private void assertEquals(String path, Scalar expected, Scalar actual) {
+		Assert.assertEquals(path + ": value should be the same", expected.getValue(), actual.getValue());
 	}
 
-	private void assertEquals(List expected, List actual) {
+	private void assertEquals(String path, List expected, List actual) {
 		Set<String> expectedValues = getValues(expected);
 		Set<String> actualValues = getValues(actual);
-		Assert.assertEquals(expectedValues, actualValues);
+		Assert.assertEquals(path + ": values should be the same", expectedValues, actualValues);
 	}
 
 	private java.util.Map<String, Value> getEntries(Map map) {
