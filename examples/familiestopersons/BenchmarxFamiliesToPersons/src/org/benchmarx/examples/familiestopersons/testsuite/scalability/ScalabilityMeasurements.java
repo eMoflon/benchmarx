bb399ea316de2bf17984bd92b8a33040b826b3c2
Tony Anjorin <tonyanjorin@googlemail.com>
2023-05-10 23:12:30 +0200

    Update ScalabilityMeasurements.java
    Remove tools that have to be setup.

--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/ScalabilityMeasurements.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/scalability/ScalabilityMeasurements.java
@@ -20,12 +20,12 @@ import Persons.PersonRegister;
 import Persons.PersonsFactory;
 
 public class ScalabilityMeasurements {
-	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool1 = new BiGULFamiliesToPersons();
+	//private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool1 = new BiGULFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool2 = new EMoflonFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool3 = new MediniQVTFamiliesToPersons();
 	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool4 = new UbtXtendFamiliesToPersons();
-	private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool5 = new FunnyQTFamiliesToPerson();
-	private static final NMFFamiliesToPersonsIncremental tool6 = new NMFFamiliesToPersonsIncremental();
+	//private static final BXTool<FamilyRegister, PersonRegister, Decisions> tool5 = new FunnyQTFamiliesToPerson();
+	//private static final NMFFamiliesToPersonsIncremental tool6 = new NMFFamiliesToPersonsIncremental();
 	
 	private static final String DELIMITER = ",";
 	private static final String UNIT = "";
@@ -34,12 +34,12 @@ public class ScalabilityMeasurements {
 	private final int NO_OF_CHILDREN;
 	private final int NO_OF_ELEMENTS;
 	
-	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer1;
+	//private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer1;
 	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer2;
 	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer3;
 	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer4;
-	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer5;
-	private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer6;
+	//private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer5;
+	//private BXToolTimer<FamilyRegister, PersonRegister, Decisions> timer6;
 	
 	public ScalabilityMeasurements(int numberOfFamilies, int noOfChildren, int repeat) {
 		this.NO_OF_FAMILIES = numberOfFamilies;
@@ -51,12 +51,12 @@ public class ScalabilityMeasurements {
 							+ 1 /* person register */
 							+ NO_OF_FAMILIES * 2*allFamilyMembers; /* person and connection to register */
 		
-		timer1 = new BXToolTimer<>(tool1, repeat);
+		//timer1 = new BXToolTimer<>(tool1, repeat);
 		timer2 = new BXToolTimer<>(tool2, repeat);
 		timer3 = new BXToolTimer<>(tool3, repeat);;
 		timer4 = new BXToolTimer<>(tool4, repeat);
-		timer5 = new BXToolTimer<>(tool5, repeat);
-		timer6 = new NMFFamiliesToPersonsTimer(tool6, repeat);
+		//timer5 = new BXToolTimer<>(tool5, repeat);
+		//timer6 = new NMFFamiliesToPersonsTimer(tool6, repeat);
 	}
 	
 	public void createPersons(PersonRegister register){
@@ -121,45 +121,45 @@ public class ScalabilityMeasurements {
 	
 	private void runBatchFWDMeasurements(){
 		System.out.print(NO_OF_ELEMENTS + DELIMITER);
-		System.out.print(timer1.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
+		//System.out.print(timer1.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
 		System.out.print(timer2.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
 		System.out.print(timer3.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
 		System.out.print(timer4.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
-		System.out.print(timer5.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
-		System.out.print(timer6.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT);
+		//System.out.print(timer5.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT + DELIMITER);
+		//System.out.print(timer6.timeSourceEditFromScratchInS(this::createFamiliesWithMembers) + UNIT);
 		System.out.println();
 	}
 	
 	private void runBatchBWDMeasurements(){
 		System.out.print(NO_OF_ELEMENTS + DELIMITER);
-		System.out.print(timer1.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
+		//System.out.print(timer1.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
 		System.out.print(timer2.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
 		System.out.print(timer3.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
 		System.out.print(timer4.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
-		System.out.print(timer5.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
-		System.out.print(timer6.timeTargetEditFromScratchInS(this::createPersons) + UNIT);
+		//System.out.print(timer5.timeTargetEditFromScratchInS(this::createPersons) + UNIT + DELIMITER);
+		//System.out.print(timer6.timeTargetEditFromScratchInS(this::createPersons) + UNIT);
 		System.out.println();
 	}
 	
 	private void runIncrFWDMeasurements(){
 		System.out.print(NO_OF_ELEMENTS + DELIMITER);
-		System.out.print(timer1.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
+		//System.out.print(timer1.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
 		System.out.print(timer2.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
 		System.out.print(timer3.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
 		System.out.print(timer4.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
-		System.out.print(timer5.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
-		System.out.print(timer6.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT);
+		//System.out.print(timer5.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT + DELIMITER);
+		//System.out.print(timer6.timeSourceEditAfterSetUpInS(this::createFamiliesWithMembers, this::createOneFamilyMember) + UNIT);
 		System.out.println();
 	}
 	
 	private void runIncrBWDMeasurements(){
 		System.out.print(NO_OF_ELEMENTS + DELIMITER);
-		System.out.print(timer1.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
+		//System.out.print(timer1.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
 		System.out.print(timer2.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
 		System.out.print(timer3.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
 		System.out.print(timer4.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
-		System.out.print(timer5.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
-		System.out.print(timer6.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT);
+		//System.out.print(timer5.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT + DELIMITER);
+		//System.out.print(timer6.timeTargetEditAfterSetUpInS(this::createPersons, this::createOnePerson) + UNIT);
 		System.out.println();
 	}
 
@@ -184,12 +184,13 @@ public class ScalabilityMeasurements {
         System.out.println(title);
         System.out.println("------------------");
         System.out.println("model size (# of nodes and edges)" + DELIMITER
-                + tool1.getName() + DELIMITER 
+                //+ tool1.getName() + DELIMITER 
                 + tool2.getName() + DELIMITER
                 + tool3.getName() + DELIMITER 
                 + tool4.getName() + DELIMITER
-                + tool5.getName() + DELIMITER
-                + tool6.getName());
+                //+ tool5.getName() + DELIMITER
+                //+ tool6.getName()
+                );
     }
 	
 	public static void main(String[] args) {
