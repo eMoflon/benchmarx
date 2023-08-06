b66683e8bbb671caff5c34b4f746f12a641dfc96
Anthony Anjorin <tonyanjorin@googlemail.com>
2023-08-06 22:13:14 +0200

    Completed monotonic creating concurrent edits

--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/concurrent/MonotonicCreating.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/testsuite/concurrent/MonotonicCreating.java
@@ -8,9 +8,9 @@ import org.junit.Test;
 import Families.FamilyRegister;
 import Persons.PersonRegister;
 
-public class Basic extends FamiliesToPersonsTestCase {
+public class MonotonicCreating extends FamiliesToPersonsTestCase {
 
-	public Basic(BXTool<FamilyRegister, PersonRegister, Decisions> tool) {
+	public MonotonicCreating(BXTool<FamilyRegister, PersonRegister, Decisions> tool) {
 		super(tool);
 	}
 
@@ -56,8 +56,8 @@ public class Basic extends FamiliesToPersonsTestCase {
 	/**
 	 * <b>Test</b> for inserting Homer as a family member and Seymour as a person
 	 * concurrently. <br/>
-	 * <b>Expect</b> : Homer must be created as a person, Seymour as a family member
-	 * in a new family.<br/>
+	 * <b>Expect</b> : Homer must be created as a male person, Seymour as a son in a
+	 * new family.<br/>
 	 * <b>Features</b>: Concurrent changes are orthogonal.
 	 */
 	@Test
@@ -66,12 +66,39 @@ public class Basic extends FamiliesToPersonsTestCase {
 
 		// ------------
 		util.configure().makeDecision(Decisions.PREFER_CREATING_PARENT_TO_CHILD, true)
-				.makeDecision(Decisions.PREFER_EXISTING_FAMILY_TO_NEW, false);
+				.makeDecision(Decisions.PREFER_EXISTING_FAMILY_TO_NEW, true);
 		tool.performAndPropagateEdit(//
 				srcEdit(helperFamily::createSimpsonFamily, helperFamily::createFatherHomer), //
 				trgEdit(helperPerson::createSeymour, helperPerson::setBirthdayOfSeymour));
 
 		util.assertPostcondition("FamilyAfterBasicConcurrentEdit3", "PersonsAfterBasicConcurrentEdit3");
+		// ------------
+	}
+
+	/**
+	 * <b>Test</b> for combined, concurrent changes. <br/>
+	 * <b>Expect</b> : Combination of matching (Homer) and orthogonal (Bart,
+	 * Seymour) cases.<br/>
+	 * <b>Features</b>: Homers are matched and not recreated, Bart and Seymour are
+	 * handled orthogonally reusing the Simpson family.
+	 */
+	@Test
+	public void testCombinedCases() {
+		tool.noPrecondition();
+
+		// ------------
+		util.configure().makeDecision(Decisions.PREFER_CREATING_PARENT_TO_CHILD, false)
+				.makeDecision(Decisions.PREFER_EXISTING_FAMILY_TO_NEW, true);
+		tool.performAndPropagateEdit(//
+				srcEdit(helperFamily::createSimpsonFamily, //
+						helperFamily::createFatherHomer, //
+						helperFamily::createSonBart), //
+				trgEdit(helperPerson::createHomer, //
+						helperPerson::createSeymour, //
+						helperPerson::changeAllBirthdays, //
+						helperPerson::setBirthdayOfSeymour));
+
+		util.assertPostcondition("FamilyAfterBasicConcurrentEdit4", "PersonsAfterBasicConcurrentEdit4");
 		// ------------
 	}
 }
