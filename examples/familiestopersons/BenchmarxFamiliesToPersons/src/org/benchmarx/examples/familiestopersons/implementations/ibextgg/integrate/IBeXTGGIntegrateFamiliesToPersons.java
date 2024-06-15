02dcb905a2e505fbdb6d42b489f5274b7d8a9f1e
Lars Fritsche <arikae00@jesitec.de>
2024-06-15 23:49:11 +0200

    now implementing the more general BXTool since we needed to write our own adapter

--- examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/implementations/ibextgg/integrate/IBeXTGGIntegrateFamiliesToPersons.java
+++ examples/familiestopersons/BenchmarxFamiliesToPersons/src/org/benchmarx/examples/familiestopersons/implementations/ibextgg/integrate/IBeXTGGIntegrateFamiliesToPersons.java
@@ -7,11 +7,14 @@ import java.util.function.Supplier;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.benchmarx.BXTool;
 import org.benchmarx.config.Configurator;
 import org.benchmarx.edit.IEdit;
 import org.benchmarx.emf.BXToolForEMF;
 import org.benchmarx.examples.familiestopersons.testsuite.Decisions;
 import org.benchmarx.families.core.FamiliesComparator;
+import org.benchmarx.families.core.FamilyHelper;
+import org.benchmarx.families.core.FamilyNormaliser;
 import org.benchmarx.persons.core.PersonsComparator;
 import org.eclipse.emf.ecore.EObject;
 import org.emoflon.ibex.tgg.run.familiestopersonsibextgg.INTEGRATE_App;
@@ -20,20 +23,20 @@ import org.emoflon.ibex.tgg.runtime.matches.ITGGMatch;
 import org.emoflon.ibex.tgg.runtime.matches.container.ImmutableMatchContainer;
 import org.emoflon.ibex.tgg.runtime.monitoring.AbstractIbexObservable;
 import org.emoflon.ibex.tgg.runtime.monitoring.IbexObservable;
+import org.junit.Assert;
 
 import Families.FamiliesFactory;
 import Families.FamilyRegister;
 import Persons.PersonRegister;
-import de.uni_koblenz.jgralab.gretl.SysOut;
+import Persons.PersonsFactory;
 
-public class IBeXTGGIntegrateFamiliesToPersons extends
-		BXToolForEMF<FamilyRegister, PersonRegister, org.benchmarx.examples.familiestopersons.testsuite.Decisions> {
+import static org.junit.Assert.*;
+
+public class IBeXTGGIntegrateFamiliesToPersons implements BXTool<FamilyRegister, PersonRegister, org.benchmarx.examples.familiestopersons.testsuite.Decisions> {
 
 	private INTEGRATE_App integrate;
 
 	public IBeXTGGIntegrateFamiliesToPersons() {
-		super(new FamiliesComparator(), new PersonsComparator());
-
 		BasicConfigurator.configure();
 		Logger.getRootLogger().setLevel(Level.ERROR);
 	}
@@ -47,25 +50,11 @@ public class IBeXTGGIntegrateFamiliesToPersons extends
 	public void initiateSynchronisationDialogue() {
 
 		try {
-//			if(integrate == null) {
-				System.out.println("-----------------------------------------");
-				System.out.println("--------------NEW INTEGRATE--------------");
-				System.out.println("-----------------------------------------");
-				integrate = new INTEGRATE_App() {
-					@Override
-					protected ITGGMatch chooseOneMatch() {
-						return getUpdatePolicy().chooseOneMatch(new ImmutableMatchContainer(getMatchContainer()));
-					}
-				};
-				
-				integrate.setUpdatePolicy(new F2PStandardUpdatePolicy());
-				
-				// Create initial src and trg models
-				FamilyRegister o = FamiliesFactory.eINSTANCE.createFamilyRegister();
-				integrate.getResourceHandler().getSourceResource().getContents().add(o);
-//			}
-
-			integrate.integrate();
+			integrate = new INTEGRATE_App();
+			
+			integrate.setUpdatePolicy(new F2PStandardUpdatePolicy());
+			
+			integrate.initialiseFamilyModel();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -74,12 +63,7 @@ public class IBeXTGGIntegrateFamiliesToPersons extends
 	@Override
 	public void performAndPropagateSourceEdit(Supplier<IEdit<FamilyRegister>> edit) {
 		// Adapt source model
-		integrate.applyDelta(new BiConsumer<EObject, EObject>() {
-			@Override
-			public void accept(EObject source, EObject target) {
-				edit.get();
-			}
-		});
+		integrate.applyDelta(integrate.applyDelta(edit, null));
 
 		// Invoke sync
 		try {
@@ -92,12 +76,7 @@ public class IBeXTGGIntegrateFamiliesToPersons extends
 	@Override
 	public void performAndPropagateTargetEdit(Supplier<IEdit<PersonRegister>> edit) {
 		// Adapt target model
-		integrate.applyDelta(new BiConsumer<EObject, EObject>() {
-			@Override
-			public void accept(EObject source, EObject target) {
-				edit.get();
-			}
-		});
+		integrate.applyDelta(integrate.applyDelta(null, edit));
 
 		// Invoke sync
 		try {
@@ -109,22 +88,12 @@ public class IBeXTGGIntegrateFamiliesToPersons extends
 
 	@Override
 	public void performIdleSourceEdit(Supplier<IEdit<FamilyRegister>> edit) {
-		integrate.applyDelta(new BiConsumer<EObject, EObject>() {
-			@Override
-			public void accept(EObject source, EObject target) {
-				edit.get();
-			}
-		});
+		integrate.applyDelta(integrate.applyDelta(edit, null));
 	}
 
 	@Override
 	public void performIdleTargetEdit(Supplier<IEdit<PersonRegister>> edit) {
-		integrate.applyDelta(new BiConsumer<EObject, EObject>() {
-			@Override
-			public void accept(EObject source, EObject target) {
-				edit.get();
-			}
-		});
+		integrate.applyDelta(integrate.applyDelta(null, edit));
 	}
 
 	@Override
@@ -134,31 +103,19 @@ public class IBeXTGGIntegrateFamiliesToPersons extends
 
 	@Override
 	public FamilyRegister getSourceModel() {
-		return (FamilyRegister) integrate.getResourceHandler().getSourceResource().getContents().get(0);
+		return integrate.getForeignSource();
 	}
 
 	@Override
 	public PersonRegister getTargetModel() {
-		return (PersonRegister) integrate.getResourceHandler().getTargetResource().getContents().get(0);
-	}
-
-	@Override
-	public void saveModels(String name) {
-		try {
-			integrate.saveModels();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		return integrate.getForeignTarget();
 	}
 
 	@Override
 	public void performAndPropagateEdit(Supplier<IEdit<FamilyRegister>> sourceEdit,
 			Supplier<IEdit<PersonRegister>> targetEdit) {
 
-		integrate.applyDelta((source, target) -> {
-			sourceEdit.get();
-			targetEdit.get();
-		});
+		integrate.applyDelta(integrate.applyDelta(sourceEdit, targetEdit));
 		
 		// Invoke sync
 		try {
@@ -175,5 +132,29 @@ public class IBeXTGGIntegrateFamiliesToPersons extends
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void assertPostcondition(FamilyRegister source, PersonRegister target) {
+		compareAndAssert(source, target);
+	}
+
+	@Override
+	public void assertPrecondition(FamilyRegister source, PersonRegister target) {
+		compareAndAssert(source, target);
+	}
+
+	private void compareAndAssert(FamilyRegister source, PersonRegister target) {
+		var actualSource = integrate.getSourceAsString();
+		var actualTarget = integrate.getTargetAsString();
+
+		var expectedSource = new FamiliesComparator().familyToString(source);
+		var expectedTarget = new PersonsComparator().personsToString(target);
+		
+		assertTrue(expectedSource.startsWith("FamilyRegister"));
+		assertEquals(expectedSource, actualSource);
+		
+		assertTrue(expectedTarget.startsWith("PersonRegister"));
+		assertEquals(expectedTarget, actualTarget);
 	}
 }
